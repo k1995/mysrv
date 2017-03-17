7ef3630db875a36c75585d359c05c5d9a8017101
k1995 <k1995328@gmail.com>
2017-03-17 22:12:09 +0800

    add asset config

--- middleware/static.js
+++ middleware/static.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const send = require('koa-send');
 
-var assetsRoot, files = [];
+var settings, files = [];
 
 /**
  * 处理静态资源
@@ -13,11 +13,11 @@ exports = module.exports = async function (ctx, next) {
 
         var maybe = ctx.path.replace('/', '');
         var tmp = maybe.split('/');
-        
-        // 判断文件夹或文件
-        if(tmp[0] == file) {
 
-            await send(ctx, ctx.path, { root: assetsRoot });
+        // 判断文件夹或文件
+        if (tmp[0] == file) {
+
+            await send(ctx, ctx.path, settings);
             return;
         }
     }
@@ -25,12 +25,12 @@ exports = module.exports = async function (ctx, next) {
     await next();
 }
 
-exports.startup = function(app) {
+exports.startup = function (app) {
 
-    const appDir = app.settings.appDir;
-    assetsRoot = path.join(appDir, 'assets');
-    if(!fs.existsSync(assetsRoot)) return;
-    files = fs.readdirSync(assetsRoot);
+    settings = app.settings.asset ? app.settings.asset : {};
+    if (!settings.root) settings.root = path.join(app.settings.appDir, 'assets');
+    if (!fs.existsSync(settings.root)) return;
+    files = fs.readdirSync(settings.root);
 }
 
 exports.level = 10;
