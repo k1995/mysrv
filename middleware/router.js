d56bf30eefbf1442224a76749af1b4a26b6a594a
k1995 <k1995328@gmail.com>
2017-04-26 22:35:33 +0800

    remove await next() when action called

--- middleware/router.js
+++ middleware/router.js
@@ -101,7 +101,6 @@ function route(controllerName) {
 
             for(let name in app.context.services) ctx[name] = app.context.services[name];
             await action.call(ctx);
-            await next();
 
         }, render.start);
     }
